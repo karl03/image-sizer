c36cee9d96addc5d3576bb2319a6add1a332aaf1
= <2566374h@student.gla.ac.uk>
2023-06-01 22:22:45 +0100

    Added basic functionality, update gitignore

--- sizer.py
+++ sizer.py
@@ -1,1 +1,17 @@
-print("hello world! :3")
+from PIL import Image, ImageOps
+
+aspect_ratio = 4/8
+
+img_path = "test.png"
+img = Image.open(img_path)
+
+if aspect_ratio < 1:
+    aspect_ratio = (1 / aspect_ratio)
+
+if (int(img.width * aspect_ratio) - img.height) < (int(img.height * aspect_ratio) - img.width):
+    img_with_border = ImageOps.expand(img,border=(0, int(((img.width * aspect_ratio) - img.height) / 2)),fill='#ffffff')
+else:
+    img_with_border = ImageOps.expand(img,border=(int(((img.height * aspect_ratio) - img.width) / 2), 0),fill='#ffffff')
+
+
+img_with_border.save('image-with-border.png')
