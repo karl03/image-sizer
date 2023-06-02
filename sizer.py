84fd2f01380a055911a28288469ec00abc42524d
= <2566374h@student.gla.ac.uk>
2023-06-02 10:58:16 +0100

    UI Improvements

--- sizer.py
+++ sizer.py
@@ -20,30 +20,38 @@ class BtnStore:
     def __init__(self):
         self.colour = "#000000"
         self.imgs_lst = ()
-        self.output_dir = ""
+        self.imgs_txt = tk.StringVar()
+        self.imgs_txt.set("0 Selected")
+        self.output_dir = tk.StringVar()
+        self.output_dir.set("None Selected")
         self.ratio_var = tk.StringVar()
 
         self.aspect_label = tk.Label(root, text = 'Aspect Ratio:')
         self.ratio_entry = tk.Entry(root,textvariable = self.ratio_var)
         self.img_sel_btn = tk.Button(root, text="Select Images", command=self.select_imgs)
+        self.imgs_label = tk.Label(root, textvariable = self.imgs_txt)
         self.colour_btn = tk.Button(root, text="Set Border Colour", command=self.select_colour)
         self.colour_dsp = tk.Button(root, bg="#000000", state="disabled")
         self.dir_sel_btn = tk.Button(root, text="Select Output Directory", command=self.select_dir)
+        self.dir_label = tk.Label(root, textvariable = self.output_dir)
         self.run_btn = tk.Button(root, text="Convert Images", command=self.convert_imgs)
 
-        self.ratio_entry.grid(row=0, column=1)
-        self.aspect_label.grid(row=0, column=0)
-        self.colour_btn.grid(row=1, column=1)
-        self.colour_dsp.grid(row=1, column=2, padx=5)
-        self.img_sel_btn.grid(row=2, column=1)
-        self.dir_sel_btn.grid(row=3, column=1)
-        self.run_btn.grid(row=4, column=1)
+        self.ratio_entry.grid(row=0, column=1, padx=5, pady=2)
+        self.aspect_label.grid(row=0, column=0, padx=5, pady=2)
+        self.colour_btn.grid(row=1, column=0, padx=5, pady=2)
+        self.colour_dsp.grid(row=1, column=1, padx=5, pady=2, sticky="nesw")
+        self.img_sel_btn.grid(row=2, column=0, padx=5, pady=2)
+        self.imgs_label.grid(row=2, column=1, padx=5, pady=2)
+        self.dir_sel_btn.grid(row=3, column=0, padx=5, pady=2)
+        self.dir_label.grid(row=3, column=1, padx=5, pady=2)
+        self.run_btn.grid(row=4, column=0, padx=5, pady=10)
 
     def select_imgs(self):
         self.imgs_lst = fd.askopenfilenames(parent=root, title="Select images", multiple=True)
+        self.imgs_txt.set(f"{len(self.imgs_lst)} Selected")
 
     def select_dir(self):
-        self.output_dir = fd.askdirectory(parent=root, title="Select output directory")
+        self.output_dir.set(fd.askdirectory(parent=root, title="Select output directory"))
 
     def select_colour(self):
         self.colour = askcolor(title="Select Colour")[1]
@@ -54,14 +62,12 @@ class BtnStore:
             img = Image.open(image)
             if image.split(".")[-1].lower() == "jpg":
                 img = img.convert("RGB")
-            expand_img(img, float(self.ratio_var.get()), self.colour).save(self.output_dir + "/" + image.split("/")[-1])
-            print("Saved as: " + self.output_dir + "/" + image.split("/")[-1])
+            expand_img(img, float(self.ratio_var.get()), self.colour).save(self.output_dir.get() + "/" + image.split("/")[-1])
+            print("Saved as: " + self.output_dir.get() + "/" + image.split("/")[-1])
 
 
 root = tk.Tk()
 root.title('Image Sizer')
-root.geometry('300x150')
-root["bg"] = "#ffffff"
 
 btnStore = BtnStore()
 
